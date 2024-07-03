89971507638d939a00e0375d1a86b2007dcd926a
Albert Martinez <161922205+Albertmmgit@users.noreply.github.com>
2024-07-03 07:31:57 +0200

    Pintar noticias
    Insertar en HTML y dar estilos a las 2 noticias inicializadas en el array de noticias

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Iblog } from '../../interfaces/iblog.interface';
 
 @Component({
   selector: 'app-blog',
@@ -8,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
+  
+  newNew: Iblog = {title: "", img: "", text: "", date: ""}
+
+  arrNews: Iblog[] = [
+    {title: "aa", img: "./images/foto1.jpeg", text: "xxxxxx", date: "2333"},
+    {title: "bb", img: "xxx", text: "xxxxxx", date: "2333"}
+   ]
+
+   loadNews() : string {
+    let html = ""
+  
+    this.arrNews.forEach((news: Iblog) => {
+      html += `
+      <article class="news">
+        <h2 class="title">${news.title}</h2>
+        <p class="text">${news.text}</p>
+        <img src="${news.img}" />
+        <p class="date">${news.date}</p>
+      </article>
+        `
+    })
+
+    return html
+  }
 
 }
